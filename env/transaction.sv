c6ac594e66b365c90b32cd51a9ffcae93eacc7dd
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-13 14:43:28 +0530

    Add UVM transaction class with addr, data, and rw fields

--- env/transaction.sv
+++ env/transaction.sv
@@ -1,8 +1,8 @@
 class transaction extends uvm_sequence_item;
 
-    bit [6:0] addr;
-    bit [7:0] data;
-    bit       rw;
+    rand bit [6:0] addr;
+    rand bit [7:0] data;
+    rand bit       rw;
 
     `uvm_object_utils_begin(transaction)
         `uvm_field_int(addr, UVM_ALL_ON)
